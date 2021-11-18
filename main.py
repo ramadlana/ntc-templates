e638882e8cee74a1905ed387190b33e30cbee6c1
ramadlana <hidayahweb@gmail.com>
2021-11-19 03:13:04 +0700

    ok

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from ntc_templates.parse import parse_output
 
-vlan_output="""
-VLAN Name                             Status    Ports
----- -------------------------------- --------- -------------------------------
-1    default                          active    Gi0/1
-10   Management                       active    
-60   VLan50                           active    Fa0/1, Fa0/2, Fa0/3, Fa0/4, Fa0/5
-                                                Fa0/6, Fa0/7, Fa0/8
-
+unparsed="""
+Interface                      Status         Protocol Description
+Vl1                            admin down     down     
+Vl99                           up             up       10.20.99.0_Switch_mgmt_VLAN
+Gi0/1                          down           down     D3 USER
+Gi0/2                          down           down     D3 USER
+Gi0/3                          down           down     D3 USER
+Gi0/4                          down           down     D3 USER
+Gi0/5                          down           down     D3 USER
+Gi0/6                          down           down     D3 USER
+Gi0/7                          down           down     D3 USER
+Gi0/8                          up             up       MERAKI TEST AP
+Gi0/9                          admin down     down     
+Gi0/10                         up             up       UPLINK TO TULCCD3S01P
+Gi0/10.10                      deleted        down
+Gi0/10.20                      up             up       Carrier VLAN
 """
-vlan_parsed = parse_output(platform="cisco_ios", command="show vlan", data=vlan_output)
-print(vlan_parsed)
+parsed = parse_output(platform="test_os", command="show test", data=unparsed)
+print(parsed)
