48cce9d6305566237cbcf7e4c31d6fc99a308597
ramadlana <hidayahweb@gmail.com>
2021-12-10 14:43:37 +0700

    ok

--- ntc_templates/run-me.py
+++ ntc_templates/run-me.py
@@ -13,28 +13,44 @@ fileLoc = os.path.join(directory, "templates")
 os.environ["NTC_TEMPLATES_DIR"] = fileLoc
 
 unparsed="""
-Route Flags: R - relay, D - download to fib
-------------------------------------------------------------------------------
-Routing Tables: Public
-         Destinations : 15       Routes : 15       
+<PE-D1-RDC-TRANSIT>display ip interface brief
+*down: administratively down
+!down: FIB overload down
+^down: standby
+(l): loopback
+(s): spoofing
+(d): Dampening Suppressed
+(E): E-Trunk down
+The number of interface that is UP in Physical is 11
+The number of interface that is DOWN in Physical is 12
+The number of interface that is UP in Protocol is 6
+The number of interface that is DOWN in Protocol is 17
 
-Destination/Mask    Proto   Pre  Cost      Flags NextHop         Interface
+Interface                         IP Address/Mask      Physical   Protocol VPN 
+Eth-Trunk0                        192.168.0.1/30       down       down     --  
+Eth-Trunk4                        172.31.104.69/31     up         up       --  
+GigabitEthernet0/0/0              10.14.19.80/24       up         up       --  
+GigabitEthernet3/0/0              unassigned           up         down     --  
+GigabitEthernet3/0/0.100          unassigned           up         down     --  
+GigabitEthernet3/0/1              unassigned           up         down     --  
+GigabitEthernet3/0/2              unassigned           up         down     --  
+GigabitEthernet3/0/2.100          unassigned           up         down     --  
+GigabitEthernet3/0/4              unassigned           down       down     --  
+GigabitEthernet3/0/4.850          unassigned           down       down     --  
+GigabitEthernet3/0/5(10G)         unassigned           down       down     --  
+GigabitEthernet3/0/5.3(10G)       192.168.2.1/30       down       down     l3vpn
+GigabitEthernet3/0/5.300(10G)     unassigned           down       down     --  
+GigabitEthernet3/0/5.3197(10G)    20.20.20.1/30        down       down     l3vpn
+GigabitEthernet3/0/5.3199(10G)    21.21.21.1/30        down       down     l3vpn
+GigabitEthernet3/0/5.3413(10G)    22.22.22.1/30        down       down     l3vpn
+GigabitEthernet3/0/5.3414(10G)    20.20.30.1/29        down       down     l3vpn
+GigabitEthernet3/0/5.3415(10G)    172.10.11.1/30       down       down     l3vpn
+LoopBack0                         1.1.1.1/32           up         up(s)    --  
+LoopBack2147483647                128.82.240.165/16    up         up(s)    l3vpn
+NULL0                             unassigned           up         up(s)    --  
+Vbdif100                          unassigned           down       down     --  
+Virtual-Template0                 unassigned           up         up(s)    --  
 
-        1.1.1.1/32  OSPF    10   65000       D   172.31.104.69   Eth-Trunk4
-        1.1.1.5/32  Direct  0    0           D   127.0.0.1       LoopBack0
-     10.14.19.0/24  Direct  0    0           D   10.14.19.23     GigabitEthernet0/0/0
-    10.14.19.23/32  Direct  0    0           D   127.0.0.1       GigabitEthernet0/0/0
-   10.14.19.255/32  Direct  0    0           D   127.0.0.1       GigabitEthernet0/0/0
-      127.0.0.0/8   Direct  0    0           D   127.0.0.1       InLoopBack0
-      127.0.0.1/32  Direct  0    0           D   127.0.0.1       InLoopBack0
-127.255.255.255/32  Direct  0    0           D   127.0.0.1       InLoopBack0
-  172.31.104.66/31  Direct  0    0           D   172.31.104.67   Eth-Trunk5
-  172.31.104.67/32  Direct  0    0           D   127.0.0.1       Eth-Trunk5
-  172.31.104.68/31  Direct  0    0           D   172.31.104.68   Eth-Trunk4
-  172.31.104.68/32  Direct  0    0           D   127.0.0.1       Eth-Trunk4
-  172.31.104.70/31  Direct  0    0           D   172.31.104.70   Eth-Trunk6
-  172.31.104.70/32  Direct  0    0           D   127.0.0.1       Eth-Trunk6
-255.255.255.255/32  Direct  0    0           D   127.0.0.1       InLoopBack0
 """
-parsed = parse_output(platform="vrp", command="display ip routing-table", data=unparsed)
+parsed = parse_output(platform="vrp", command="display ip interface brief", data=unparsed)
 print(json.dumps(parsed, indent=4))
